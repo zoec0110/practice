82ae097f3071620fb7cfc1d42b3bc56de552c5c8
Yin <zoe@statementdog.com>
2022-09-30 15:55:35 +0800

    finish revising wehelp_3

--- wehelp_3.rb
+++ wehelp_3.rb
@@ -3,24 +3,19 @@
 
 # Python
 # def maxProduct(nums):
-    # 請用你的程式補完這個函式的區塊
-    # maxProduct([5, 20, 2, 6]) # 得到 120
-    # maxProduct([10, -20, 0, 3]) # 得到 30
-    # maxProduct([-1, 2]) # 得到 -2
-    # maxProduct([-1, 0, 2]) # 得到 0
-    # maxProduct([-1, -2, 0]) # 得到 2
+# 請用你的程式補完這個函式的區塊
+# maxProduct([5, 20, 2, 6]) # 得到 120
+# maxProduct([10, -20, 0, 3]) # 得到 30
+# maxProduct([-1, 2]) # 得到 -2
+# maxProduct([-1, 0, 2]) # 得到 0
+# maxProduct([-1, -2, 0]) # 得到 2
 
 def max_product(nums)
-  max = 0
   sort_array = nums.sort
   len = nums.length
   first_product = sort_array[0] * sort_array[1]
   last_product = sort_array[len - 1] * sort_array[len - 2]
-  if first_product > last_product
-    max = first_product
-  else
-    max = last_product
-  end
+  max = first_product > last_product ? first_product : last_product
   puts max
 end
 
