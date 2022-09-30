bbec1490fb4b240e461be3bb0abe9932b7df9a95
莊尹淳 <yin@zhuangyinchundeMacBook-Pro.local>
2022-09-30 12:03:40 +0800

    add homework_2

--- wehelp_3.rb
+++ wehelp_3.rb
@@ -3,12 +3,12 @@
 
 # Python
 # def maxProduct(nums):
-  # 請用你的程式補完這個函式的區塊
-  # maxProduct([5, 20, 2, 6]) # 得到 120
-  # maxProduct([10, -20, 0, 3]) # 得到 30
-  # maxProduct([-1, 2]) # 得到 -2
-  # maxProduct([-1, 0, 2]) # 得到 0
-  # maxProduct([-1, -2, 0]) # 得到 2
+    # 請用你的程式補完這個函式的區塊
+    # maxProduct([5, 20, 2, 6]) # 得到 120
+    # maxProduct([10, -20, 0, 3]) # 得到 30
+    # maxProduct([-1, 2]) # 得到 -2
+    # maxProduct([-1, 0, 2]) # 得到 0
+    # maxProduct([-1, -2, 0]) # 得到 2
 
 def max_product(nums)
   max = 0
